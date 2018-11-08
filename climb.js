80c87362e40f162df292f909010b5dd99fd686a9
Justin Venezuela <jven@jvenezue.la>
2018-11-08 15:37:07 -0500

    Makes a rectangle fall from the sky.

--- climb.js
+++ climb.js
@@ -1,150 +1,36 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 300 },
-        debug: false
-      }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
-
-var game = new Phaser.Game(config);
-var score = 0;
-var scoreText;
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
-  this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
-  );
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-
-  platforms = this.physics.add.staticGroup();
-
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
-
-  stars = this.physics.add.group({
-    key: 'star',
-    repeat: 11,
-    setXY: {
-      x: 12,
-      y: 0,
-      stepX: 70
-    }
-  });
-
-  stars.children.iterate(function(child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
-
-  bombs = this.physics.add.group();
-
-  player = this.physics.add.sprite(100, 450, 'dude');
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
-  this.physics.add.collider(bombs, platforms);
-  this.physics.add.collider(player, bombs, hitBomb, null, this);
-
-  this.physics.add.overlap(player, stars, collectStar, null, this);
-
-  cursors = this.input.keyboard.createCursorKeys();
-
-  scoreText = this.add.text(
-      16,
-      16,
-      'Score: 0',
-      {
-        fontSize: '32px',
-        fill: '#000'
-      });
-}
-
-function update () {
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-  } else {
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-  }
-}
-
-function collectStar(player, star) {
-  star.disableBody(true, true);
-  score += 10;
-  scoreText.setText('Score: ' + score);
-
-  if (stars.countActive(true) == 0) {
-    stars.children.iterate(function(child) {
-      child.enableBody(true, child.x, 0, true, true);
+var game = new Phaser.Game(
+    800 /* width */,
+    600 /* height */,
+    Phaser.AUTO,
+    '',
+    {
+      create: createFn,
+      update: updateFn
     });
 
-    var bombX = (player.x < 400) ?
-        Phaser.Math.Between(400, 800) :
-        Phaser.Math.Between(0, 400);
-    var bomb = bombs.create(bombX, 16, 'bomb');
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    bomb.allowGravity = false;
-  }
+function createFn() {
+  game.physics.startSystem(Phaser.Physics.P2JS);
+  game.physics.p2.gravity.y = 1000;
+
+  var graphics = game.add.graphics(-100, -100);
+  graphics.beginFill(0xFF0000);
+  graphics.drawRect(0, 0, 20, 30);
+  graphics.endFill();
+  var texture = graphics.generateTexture();
+  game.cache.addSpriteSheet(
+      'rect',
+      null,
+      texture.baseTexture.source,
+      20,
+      30,
+      1,
+      0,
+      0);
+  
+  // var group = game.add.physicsGroup(Phaser.Physics.P2JS);
+  var fallingRect = game.add.sprite(300, 100, 'rect');
+  game.physics.p2.enable([fallingRect], false);
+
 }
 
-function hitBomb(player, bomb) {
-  this.physics.pause();
-  player.setTint(0xff0000);
-  player.anims.play('turn');
-}
+function updateFn () {}
