2b32a8d803a783976a605394418f7d6baf195dd9
Justin Venezuela <jven@jvenezue.la>
2018-11-09 12:41:30 -0500

    Adds a background image and a camera following the climber.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 var game = new Phaser.Game(
-    800 /* width */,
+    400 /* width */,
     600 /* height */,
     Phaser.AUTO,
     '',
     {
+      preload: preloadFn,
       create: createFn,
       update: updateFn
     });
 var climber;
 
+function preloadFn() {
+  game.load.image('background', 'bg.png');
+};
+
 function createFn() {
+  game.add.tileSprite(0, 0, 1920, 1920, 'background');
+  game.world.setBounds(0, 0, 1920, 1920);
+
   game.physics.startSystem(Phaser.Physics.P2JS);
   game.physics.p2.gravity.y = 1000;
 
@@ -17,6 +25,8 @@ function createFn() {
   climber.moveEntireBodyTo(300, 350);
 
   game.input.onDown.add(onMouseDown, this);
+
+  game.camera.follow(climber.upperBody_);
 }
 
 function updateFn() {}
