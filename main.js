00b6ce070393f575e036816cc2c60685aa0e6439
DouglasDev <doug012310@yahoo.com>
2018-11-09 16:31:28 -0500

    fixed camera and mouse dragging

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var game = new Phaser.Game(
     });
 var climber;
 var course;
-
+var scale=.5;
 function preloadFn() {
   game.load.image('background', 'bg.png');
 };
@@ -27,22 +27,38 @@ function createFn() {
 
   course = new cyberpunks.Course(game);
 
-  game.input.onDown.add(onMouseDown, this);
+ // game.input.onDown.add(onMouseDown, this);
 
   game.camera.follow(climber.upperBody_);
-  game.camera.scale.x = 0.5;
-  game.camera.scale.y = 0.5;
+  game.camera.scale.x = scale;
+  game.camera.scale.y = scale;
+
+
+    game.input.addMoveCallback(move, this);
+
+
 }
 
 function updateFn() {
 //console.log(game.physics.p2)
 climber.leftHand_.body.setZeroVelocity();
 //    climber.leftHand_.body.static=true;
-    climber.leftHand_.body.x=game.input.mousePointer.x;
-    climber.leftHand_.body.y=game.input.mousePointer.y;
+    //climber.leftHand_.body.x=game.input.mousePointer.x;
+    climber.leftHand_.body.x=game.input.activePointer.worldX/scale;
+    climber.leftHand_.body.y=game.input.activePointer.worldY/scale;
 
 
 }
+
+
+function move(pointer) {
+ var physicsPos = [game.physics.p2.pxmi(pointer.position.x), game.physics.p2.pxmi(pointer.position.y)];
+    // p2 uses different coordinate system, so convert the pointer position to p2's coordinate system
+  //  climber.leftHand_.body.x = physicsPos[0];
+ //   climber.leftHand_.body.y = physicsPos[1];
+
+}
+
 
 function onMouseDown(pointer) {
   var clickedBodyParts = game.physics.p2.hitTest(
