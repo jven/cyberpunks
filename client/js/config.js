2d97ee3aecfafe84fda913753ec6abaccb72f195
Justin Venezuela <jven@jvenezue.la>
2018-11-13 13:02:30 -0500

    Moves some constants to Config.

--- client/js/config.js
+++ client/js/config.js
@@ -23,3 +23,13 @@ cyberpunks.Config.GAME_HEIGHT = 1920;
 // How far the camera is zoomed.
 // 1.0 is normal, 0.5 is more zoomed out, 2.0 is more zoomed in.
 cyberpunks.Config.CAMERA_SCALE = 0.75;
+
+// The width of the camera's deadzone. If the climber is within this margin from
+// the edge of the camera, the camera will scroll.
+cyberpunks.Config.CAMERA_DEADZONE_WIDTH = 100;
+
+// The amount of gravity in the y-direction.
+cyberpunks.Config.GRAVITY_Y = 1000;
+
+// The size of the climber. A bigger number is a bigger size.
+cyberpunks.Config.CLIMBER_SIZE = 100;
